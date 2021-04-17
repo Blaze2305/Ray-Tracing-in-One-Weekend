b6738e213d609e2ae4f544832a8400686269d8ad
Pranav <prajpran@gmail.com>
2021-04-17 19:37:19 +0530

    ray sphere intersection done

--- src/Ray/ray.cpp
+++ src/Ray/ray.cpp
@@ -17,7 +17,32 @@ point3 ray::at(double t) const{
 // ------------------------------------------------
 
 color ray_color(const ray& r){
+	if(hit_sphere(point3(0,0,1),0.6,r)){
+		return color(1,0,0);
+	}
 	vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
+
+bool hit_sphere(const point3 & center,double radius,const ray& r){
+	// expanded form of the vector equation of the sphere
+	// ==> t^2(b.b) + 2tb.(A-C) + (A - C).(A - C) - r^2 = 0
+	// where C is the center of the sphere
+
+	// oc is equal to (A - C)
+	vec3 oc = r.origin() - center;
+
+	// a => b . b where b = ray direction vec
+	double a = dot(r.direction(),r.direction());
+
+	// b => 2 * b . (A - C) , b = ray direction vec
+	double b = 2 * dot(r.direction(),oc);
+
+	// c => (A - C).(A - C) - radius^2
+	double c = dot(oc,oc) - radius * radius;
+
+	double discriminant = b * b - 4 * a * c;
+	// reutrn true if roots exist
+	return discriminant>0;
 }
--- src/Ray/ray.h
+++ src/Ray/ray.h
@@ -31,5 +31,17 @@ class ray{
 //  			blendedValue=(1−t)⋅startValue+t⋅endValue,
 color ray_color(const ray& r);
 
+// Function to check if the ray intersects the sphere with a given center and radius.
+// The math for the function is explained in the link below
+// https://raytracing.github.io/books/RayTracingInOneWeekend.html#addingasphere/ray-sphereintersection
+// But TL;DR:  equation of a sphere is (x-Cx)^2 + (y-Cy)^2 + (z-Cz)^2 = r^2  for a sphere with radius r and center C(Cx,Cy,Cz)
+// The vector form is  (P-C).(P-C) = r^2 [ note . is the dot product]
+// but P = (A + t*b)  where A = origin of the ray and B is the direction vector
+//  so the vector equation of the circle becomes  (A+ tb - C).(A + tb -C) = r^2
+// expanding this gives us t^2(b.b) + 2tb.(A-C) + (A - C).(A - C) - r^2 = 0
+// this is of the form a^2 + b^2 + 2ab = 0; a  quadratic equation
+// So if the ray intersects the sphere there shoudl exists roots for the equation to give the values of t
+// for roots to exist we check the determinant D = b^2 - 4ac. if D > 0 then it intersects
+bool hit_sphere(const point3& center, double radius, const ray& r);
 
 #endif
